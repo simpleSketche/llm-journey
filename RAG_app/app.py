d5754195f5498524dde35030d965e536131a3c26
SimpleSketche <yangyankun.work@gmail.com>
2025-01-12 00:11:32 -0500

    update a tip

--- RAG_app/app.py
+++ RAG_app/app.py
@@ -1,5 +1,8 @@
 from pdf_extractor import extract_from_pdf_pages
 
+# Tip: if the pdf you found is shown in the pdf viewer on a website,
+# open the dev Element inspector, and find the html element class named / id named -> "download",
+# it's hidden by default, add / change the display style property to "inline" from "hidden".
 pdf_file_path = "pdf_data/2022BC_NYC_BuildingCodes_Definitions.pdf"
 
 def run():
